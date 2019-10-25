c6371464daaace7c062d8e4c7559ab25282fc46a
lauvrenn <lauvrenn@gmail.com>
2019-10-25 19:20:17 +0200

    Added Tailwind, autoprefixer, postcss

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -12,4 +12,5 @@ const Search = () => {
   );
 };
 
+
 export default withLayout(Search);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import withLayout from '../components/Layout';
 import Link from 'next/link';
+
 
 const PostLink = (props: any) => (
     <li>
@@ -9,10 +11,13 @@ const PostLink = (props: any) => (
     </li>
   );
 
-const Index = () => (
-    <ul>
-        <PostLink id="dynamic-link" />
-    </ul>
-);
+class Index extends React.Component {
+  render() {
+    return (
+      <></>
+      // <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" placeholder="Hi there!">
+    )
+  }
+}
 
 export default withLayout(Index);
