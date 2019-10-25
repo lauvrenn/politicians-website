7aa0e9fa25446074d6037cf4e87e5d167f0ef9de
lauvrenn <lauvrenn@gmail.com>
2019-10-25 20:01:56 +0200

    scripts, prettier, webpack, Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,11 @@
 import React from 'react'
-import withLayout from '../components/Layout';
-import Link from 'next/link';
-
-
-const PostLink = (props: any) => (
-    <li>
-      <Link href={'/[id]'} as={`/${props.id}`}>
-        <a>{props.id}</a>
-      </Link>
-    </li>
-  );
-
+import Main from './main'
 class Index extends React.Component {
   render() {
     return (
-      <></>
-      // <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="text" placeholder="Hi there!">
+     <Main/>
     )
   }
 }
 
-export default withLayout(Index);
+export default Index;
